fd4ebacee29b9661f9f5a781ecf5e49dbc75d2bf
hjpxt <hjpxt@qq.com>
2021-11-26 16:54:33 +0800

    first commit

---
+++ src/main/java/com/hjp/GitTest.java
@@ -0,0 +1,21 @@
+package com.hjp;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
