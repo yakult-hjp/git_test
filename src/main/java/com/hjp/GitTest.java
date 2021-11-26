6415f67e77d1aa123e3d3e4c8ed10ba540cd7d2a
hjpxt <hjpxt@qq.com>
2021-11-26 17:02:55 +0800

    hot_fix commit

--- src/main/java/com/hjp/GitTest.java
+++ src/main/java/com/hjp/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello1");
     }
 }
